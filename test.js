10d908a225797cf567388d073cb57a74eafc5007
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-11-24 18:53:09 -0800

    Added unit test for symlinking

--- test.js
+++ test.js
@@ -3,13 +3,20 @@ var assert = require('assert');
 var fs = require('fs');
 var rimraf = require('rimraf');
 
-afterEach(function () {
+after(function () {
   rimraf.sync('temp');
 });
 
-it('should not alterate content', function () {
+it('does not alterate content', function () {
   assert.equal(
     fs.readFileSync('temp/lorem.txt', 'utf8'),
     fs.readFileSync('fixture/lorem.txt', 'utf8')
+  );
+});
+
+it('symlinks files, not copy them', function () {
+  assert.equal(
+    fs.realpathSync('temp/lorem.txt'),
+    fs.realpathSync('fixture/lorem.txt')
   );
 });
