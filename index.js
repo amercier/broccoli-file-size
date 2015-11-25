b5f677008786f79e9f83f116eadae18cb71b8a13
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-11-24 20:29:03 -0800

    Symlinked/copied files instead of reading/writing them

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 var chalk = require('chalk'),
   Filter = require('broccoli-filter'),
   filesize = require('filesize'),
+  fs = require('fs'),
   merge = require('merge'),
+  mkdirp = require('mkdirp'),
+  path = require('path'),
   RSVP = require('rsvp'),
+  symlinkOrCopy = require('symlink-or-copy'),
   zlib = require('zlib');
 
 function FileSizeFilter(inputTree, options) {
@@ -25,7 +29,38 @@ FileSizeFilter.prototype.getDestFilePath = function (relativePath) {
   return relativePath;
 };
 
-FileSizeFilter.prototype.processString = function (content, relativePath) {
+Filter.prototype.processFile = function processFile(srcDir, destDir, relativePath) {
+  return this.readFile(path.join(srcDir, relativePath))
+  .then(function(contents) {
+    return this.processString(contents, relativePath);
+  }.bind(this))
+  .then(function asyncOutputFilteredFile() {
+    var outputPath = this.getDestFilePath(relativePath);
+    if (outputPath == null) {
+      throw new Error('canProcessFile("' + relativePath + '") is true, but getDestFilePath("' + relativePath + '") is null');
+    }
+    mkdirp.sync(path.dirname(outputPath));
+    this.copy(
+      path.join(srcDir, relativePath),
+      path.join(destDir, outputPath)
+    );
+  }.bind(this));
+};
+
+FileSizeFilter.prototype.readFile = function(path) {
+  return new RSVP.Promise(function(resolve, reject) {
+    fs.readFile(path, function(err, contents) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(contents);
+      }
+    });
+  }.bind(this));
+};
+
+FileSizeFilter.prototype.processString = function(content, relativePath) {
   var message = relativePath && chalk.yellow(relativePath)
     + ' => ' + chalk.green(filesize(content && content.length)),
     options = this.options;
@@ -45,8 +80,25 @@ FileSizeFilter.prototype.processString = function (content, relativePath) {
     })
     .then(function(message) {
       process.stdout.write(options.colors ? message : chalk.stripColor(content));
-      return content;
     });
+};
+
+FileSizeFilter.prototype.copy = function(sourcePath, destPath) {
+  var destDir = path.dirname(destPath);
+
+  try {
+    symlinkOrCopy.sync(sourcePath, destPath);
+  } catch(e) {
+    if (!fs.existsSync(destDir)) {
+      mkdirp.sync(destDir);
+    }
+    try {
+      fs.unlinkSync(destPath);
+    } catch(e) {
+
+    }
+    symlinkOrCopy.sync(sourcePath, destPath);
+  }
 };
 
 module.exports = FileSizeFilter;
