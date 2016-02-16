590092db101e46e87ae068da8e16267e9a366e22
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-02-16 15:29:45 -0600

    Refactored further and added code documentation

--- src/index.js
+++ src/index.js
@@ -13,8 +13,16 @@ import walk from 'walk';
 
 const [preadFile, pstat, pgzip] = [readFile, stat, gzip].map(promisify);
 
+/**
+ * A Broccoli plugin that display sizes of all files found in its input path.
+ */
 export default class FileSizePlugin extends Plugin {
 
+  /**
+   * Create a new FileSizePlugin instances
+   * @param {Node|Node[]} inputNodes Input nodes, see `broccoli-plugin` docs
+   * @param {Object} options Plugin options
+   */
   constructor(inputNodes, options) {
     super(isArray(inputNodes) ? inputNodes : [inputNodes], options);
 
@@ -24,19 +32,40 @@ export default class FileSizePlugin extends Plugin {
     }, options);
   }
 
+  /**
+   * Builds this node.
+   *
+   * Symlinks the first input path to this node's output path, then look for all
+   * files in the input path, and process each of them by either:
+   * - reading the file's contents and calculating its gzipped sie (when
+   *   `gzipped` options is `true`)
+   * - or, stating the file (if `gzipped` option is `false`).
+   * Then, display size(s) on standard output.
+   *
+   * @return {Promise} A new Promise that is resolved once all input files have
+   * been processed, and sizes have been displayed. Alternatively, it can be
+   * rejected in case of errors.
+   */
   build() {
     const [inputPath] = this.inputPaths;
 
     // Symlink/copy input -> output
-    this.copy(inputPath, this.outputPath);
+    this.symlinkOrCopy(inputPath, this.outputPath);
 
     // Process output directory
     return this.listFiles(inputPath, relativePath =>
-      this.processFile(inputPath, relativePath)
+      this.processFile(join(inputPath, relativePath))
         .then(sizes => this.print(relativePath, ...sizes))
     );
   }
 
+  /**
+   * Lists files of a directory recursively. Follows symbolic links.
+   * @param {String} dir Directory to scan
+   * @param {Function()} callback Callback executed when a file is found. Can return a Promise.
+   * @return {Promise} A new promise that is resolved once the given directory
+   * has been scanned entirely and all callbacks have completed.
+   */
   listFiles(dir, callback) {
     return new Promise((resolveThis, reject) => {
       walk.walk(dir, { followLinks: true })
@@ -50,39 +79,49 @@ export default class FileSizePlugin extends Plugin {
     });
   }
 
-  copy(inputPath, outputPath) {
+  /**
+   * Symlink or copy a directory
+   * @param {String} dir Path to an existing
+   * @param {String} target Path of the symlink to create
+   */
+  symlinkOrCopy(dir, target) {
     try {
-      symlinkOrCopy.sync(inputPath, outputPath);
+      symlinkOrCopy.sync(dir, target);
     } catch (e) {
-      if (existsSync(outputPath)) {
-        rimraf.sync(outputPath);
+      if (existsSync(target)) {
+        rimraf.sync(target);
       }
-      symlinkOrCopy.sync(inputPath, outputPath);
+      symlinkOrCopy.sync(dir, target);
     }
   }
 
-  processFile(dir, relativePath) {
-    const absolutePath = join(dir, relativePath);
-    if (this.options.gzipped) {
-      return preadFile(absolutePath)
-        .then(contents => this.processString(relativePath, contents));
+  /**
+   * Either reads the given file's contents and calculating its gzipped sie
+   * (when `gzipped` options is `true`), or stats the given file
+   * (if `gzipped` option is `false`).
+   * @param {String} absolutePath Absolute path to the file
+   * @return {Promise} A new Promise that is resolved when file has been
+   * processed. The resolved value is an array containing the file size and the
+   * gzipped file size (or undefined)
+   */
+  processFile(absolutePath) {
+    // Stats the file if `gzipped` option is set to false
+    if (!this.options.gzipped) {
+      return pstat(absolutePath).then(stats => [stats.size]);
     }
-    return pstat(absolutePath)
-      .then(stats => this.processStats(relativePath, stats));
+
+    // Otherwise, reads the file contents and calculate gzipped size
+    return preadFile(absolutePath)
+      .then(contents => [contents, pgzip(contents)])
+      .then(buffers => buffers.map(buffer => buffer.toString().length));
   }
 
-  processStats(relativePath, stats) {
-    return resolve([stats.size]);
-  }
-
-  processString(relativePath, content) {
-    return pgzip(content)
-      .then(gzippedContent => [
-        content.toString().length,
-        gzippedContent.toString().length,
-      ]);
-  }
-
+  /**
+   * Prints calculated sizes
+   * @param {String} relativePath Relative path of the input file
+   * @param {Number]} size Size of the input file in bytes
+   * @param {Number} [gzippedSize] Size of the gzipped version of the file
+   */
   print(relativePath, size, gzippedSize) {
     let message = `${yellow(relativePath)} => ${green(filesize(size))}`;
     if (!isUndefined(gzippedSize)) {
