8f7597a6007c2fd11450513ce8b1a9c89277004c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-05-26 11:38:36 -0700

    Imported code and specs

--- specs/lib/index.spec.js
+++ specs/lib/index.spec.js
@@ -1,13 +1,43 @@
-import { expect } from 'chai';
-import myModule from '../../src/lib/index';
+/* eslint-env mocha */
 
-/** @test {myModule} */
-describe('myModule', () => {
-  it('exists', () => {
-    expect(myModule).to.exist;
-  });
+import fs from 'fs';
+import { join } from 'path';
 
-  it('is true', () => {
-    expect(myModule).to.be.true;
-  });
+import assert from 'assert';
+import { Builder } from 'broccoli';
+import bufferEqual from 'buffer-equal';
+
+import FileSizePlugin from '../../src/lib';
+const fixtureDir = join(__dirname, '..', 'fixture');
+
+let tree;
+before(() => {
+  tree = new FileSizePlugin(
+    new FileSizePlugin([fixtureDir]),
+    { gzipped: false, colors: false }
+  );
+  return new Builder(tree).build();
+});
+
+it('does not alter text files', () => {
+  assert.equal(
+    fs.readFileSync(join(tree.outputPath, 'lorem.txt'), 'utf8'),
+    fs.readFileSync(join(fixtureDir, 'lorem.txt'), 'utf8')
+  );
+});
+
+it('does not alter binary files', () => {
+  assert.ok(
+    bufferEqual(
+      fs.readFileSync(join(tree.outputPath, 'broccoli-logo.generated.png')),
+      fs.readFileSync(join(fixtureDir, 'broccoli-logo.generated.png'))
+    )
+  );
+});
+
+it('does not alter paths', () => {
+  assert.equal(
+    fs.readFileSync(join(tree.outputPath, 'subdir', 'other.txt'), 'utf8'),
+    fs.readFileSync(join(fixtureDir, 'subdir', 'other.txt'), 'utf8')
+  );
 });
